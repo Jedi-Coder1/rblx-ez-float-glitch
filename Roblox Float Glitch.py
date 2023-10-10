85598af4d8f14e1166e6d423ed44b52f5f49a8f1
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-10-10 08:29:59 -0600

    Update Roblox Float Glitch.py

--- Roblox Float Glitch.py
+++ Roblox Float Glitch.py
@@ -1,13 +1,19 @@
 import win32gui
 import win32api
 import win32con
+import keyboard
 
 win2find = "Roblox"
 
+def keyboardInput(typ):
+    whnd = win32gui.FindWindowEx(None, None, None, win2find)
+    if not (whnd == 0):
+        print('Found')
 
-
-
-whnd = win32gui.FindWindowEx(None, None, None, win2find)
-if not (whnd == 0):
-    print('Found')
-    
+while True:
+    if keyboard.is_pressed('y'):
+        print('Freezing')
+        keyboardInput("Freeze")
+    elif keyboard.is_pressed('u'):
+        print('Unfrozen')
+        keyboardInput("Unfreeze")
