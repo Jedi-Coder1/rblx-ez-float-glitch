daa65726fa83ad7cd10a03986291cd5b53813ac6
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-10-10 10:36:48 -0600

    Update Roblox Float Glitch.py

--- Roblox Float Glitch.py
+++ Roblox Float Glitch.py
@@ -1,14 +1,34 @@
 import win32gui
 import win32api
 import win32con
+import keyboard
 
 win2find = "Roblox"
 
-def Freezer():
-    whnd = win32gui.FindWindowEx(None, None, None, win2find)
-    if not (whnd == 0):
-        print('Found')
-    else:
-        print('Roblox Not Opened')
+class Freeze():
+    def __init__(self) -> None:
+        self.canPress = True
+    
+    def click(self, x, y):
+        hWnd = win32gui.FindWindow(None, win2find)
+        lParam = win32api.MAKELONG(x, y)
 
-Freezer()
+        hWnd1= win32gui.FindWindowEx(hWnd, None, None, None)
+        win32gui.SendMessage(hWnd1, win32con.WM_RBUTTONDOWN, win32con.MK_RBUTTON, lParam)
+        win32gui.SendMessage(hWnd1, win32con.WM_RBUTTONUP, None, lParam)
+
+
+    def Freezer(self):
+        if self.canPress:
+            whnd = win32gui.FindWindowEx(None, None, None, win2find)
+            if not (whnd == 0):
+                print('Found')
+                self.click(100,100)
+            else:
+                print('Roblox Not Opened')
+
+
+keyboard.add_hotkey('y', Freeze().Freezer)
+
+while True:
+    keyboard.read_key()
