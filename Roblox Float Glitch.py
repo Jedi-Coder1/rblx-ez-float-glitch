4d72c2ba9dd3b1a5dce0febd682b9801de98dccc
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-10-10 09:28:36 -0600

    Update Roblox Float Glitch.py

--- Roblox Float Glitch.py
+++ Roblox Float Glitch.py
@@ -1,11 +1,14 @@
 import win32gui
 import win32api
 import win32con
-import keyboard
 
 win2find = "Roblox"
 
-def keyboardInput():
+def Freezer():
     whnd = win32gui.FindWindowEx(None, None, None, win2find)
     if not (whnd == 0):
         print('Found')
+    else:
+        print('Roblox Not Opened')
+
+Freezer()
