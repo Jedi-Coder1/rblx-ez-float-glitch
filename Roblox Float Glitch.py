65cdd81d3c617640f003014aca133bbca898dadb
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-10-10 08:14:46 -0600

    Update Roblox Float Glitch.py

--- Roblox Float Glitch.py
+++ Roblox Float Glitch.py
@@ -1,6 +1,13 @@
 import win32gui
+import win32api
+import win32con
 
 win2find = "Roblox"
+
+
+
+
 whnd = win32gui.FindWindowEx(None, None, None, win2find)
 if not (whnd == 0):
-    print('FOUND!')
+    print('Found')
+    
